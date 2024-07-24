80a93f29938d199b8f39ab8e699132dcf7b0f7e4
Andrew Easley <easleyandrewp@gmail.com>
2024-07-24 13:36:49 -0500

    Initial Player Movement and Sprites

---
+++ Scripts/player.gd
@@ -0,0 +1,93 @@
+##player.gd
+
+extends CharacterBody2D
+
+##ANIMATION 
+@onready var animation_sprite = $AnimatedSprite2D
+var animation : String
+
+## MOVEMENT 
+@export var base_speed = 50
+var movement_speed = base_speed
+
+var direction_input : Vector2
+var current_direction : Vector2
+
+##PLAYER STATS
+
+##PLAYER STATE 
+var is_dashing = false
+
+
+
+func _ready():
+	movement_speed = base_speed
+
+func _physics_process(delta):
+	direction_input.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	direction_input.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	
+	if abs(direction_input.x) == 1 and abs(direction_input.y) == 1:
+		direction_input = direction_input.normalized()
+	
+	if Input.is_action_pressed("ui_sprint"):
+		movement_speed = base_speed * 1.5
+	elif Input.is_action_just_released("ui_sprint"):
+		movement_speed = base_speed
+	
+	if Input.is_action_just_pressed("ui_roll"):
+		is_dashing = true
+		movement_speed = base_speed * 3
+	
+	var movement = movement_speed * direction_input * delta
+	
+	
+	move_and_collide(movement)
+	player_animations(direction_input)
+
+
+func player_animations(update_direction: Vector2):
+	if update_direction != Vector2.ZERO:
+		current_direction = update_direction
+		if is_dashing:
+			animation = "roll_" + return_direction(current_direction)
+		else:
+			animation = "walk_" + return_direction(current_direction)
+	else:
+		animation  = "idle_" + return_direction(current_direction)
+	
+	animation_sprite.play(animation)
+		
+
+func return_direction(direction: Vector2):
+	var default_return = "south"
+	var angle = direction.angle()
+	var returned_direction : String = ""
+	
+	if angle >= - PI/8 and angle < PI/8:
+		returned_direction = "east"
+	elif angle >= PI/8 and angle < 3*PI/8:
+		returned_direction = "southeast"
+	elif angle >= 3*PI/8 and angle < 5*PI/8:
+		returned_direction = "south"
+	elif angle >= 5*PI/8 and angle < 7*PI/8:
+		returned_direction = "southwest"
+	elif angle >= 7*PI/8 and angle < -7*PI/8:
+		returned_direction = "west"
+	elif angle >= -7*PI/8 and angle < -5*PI/8:
+		returned_direction = "northwest"
+	elif angle >= -5*PI/8 and angle < -3*PI/8:
+		returned_direction = "north"
+	elif angle >= -3*PI/8 and angle < -PI/8:
+		returned_direction = "northeast"
+	else:
+		returned_direction = default_return
+	
+	return returned_direction
+	
+
+
+func _on_animated_sprite_2d_animation_finished():
+	if is_dashing:
+		is_dashing = false
+		movement_speed = base_speed
