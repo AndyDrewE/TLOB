9cb64f0d6c39ba5adb6518ece55f275c290b8dab
Andrew Easley <easleyandrewp@gmail.com>
2024-07-26 15:12:50 -0500

    Spawning items works in isolation

--- Scenes/ItemController.gd
+++ Scenes/ItemController.gd
@@ -6,7 +6,8 @@ var items = {}
 
 func _ready():
 	load_all_items()
-	print(items)
+	spawn_item(0,Vector2(100,100))
+	
 	
 # Function to load item resources, will deprecate into database
 func load_all_items():
@@ -20,4 +21,21 @@ func load_all_items():
 		if item:
 			items[item.ID] = item
 
+# Get item by item ID
+func get_item(item_ID : int) -> Item:
+	if item_ID in items:
+		return items[item_ID]
+	
+	print("Item ID (%s) not found" % item_ID)
+	return null
 
+#Spawn item at given position
+func spawn_item(item_ID : int, spawn_location : Vector2) -> ItemNode:
+	var item_resource = get_item(item_ID)
+	if item_resource:
+		var new_item_node = ItemNode.new()
+		new_item_node.init(item_resource)
+		new_item_node.position = spawn_location
+		get_parent().add_child.call_deferred(new_item_node)
+		return new_item_node
+	return null
--- Scripts/item_node.gd
+++ Scripts/item_node.gd
@@ -1,32 +1,30 @@
 ## item_node.gd
-extends Node2D
 
 class_name ItemNode
 
-@onready var item_icon = $item_icon
+extends Node2D
 
-var item : Item
+var item_icon : TextureRect
+
+var item : Item 
+
 
 func _ready():
 	if item != null:	
 		update_item_texture()
 
-#func init():
-	#var item_path : String
-	#if randi() % 2 == 0:
-		#item_path = "res://Data/item_resources/apple.tres"
-	#else:
-		#item_path = "res://Data/weapon_resources/melee/basic_sword.tres"
-#
-	#item = load(item_path) as Item
-	#
-	#if item == null:
-		#print("Failed to load item from path: %s" % item_path)
-	#else:
-		#update_item_texture()
+func init(p_item : Item):
+	item = p_item
+	if is_inside_tree():
+		update_item_texture()
+	else:
+		call_deferred("update_item_texture")
 
 func update_item_texture():
+	if item_icon == null:
+		item_icon = TextureRect.new()
+		add_child(item_icon)
 	if item != null:
-		$item_icon.texture = item.texture
+		item_icon.texture = item.texture
 	else:
 		print("No item to set texture for")
